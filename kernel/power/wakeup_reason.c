4a5634bd44654c6915edab9173a192993c0110d9
Wei Wang <wvw@google.com>
2016-10-27 11:03:12 -0700

    power: wakeup_reason: make log function work in interrupt context
    If log_suspend_abort_reason called in interrupt context, the spinlock
    may deadlock.
    With https://lkml.org/lkml/2014/9/1/404, wakeup mechanism changed, so
    log_suspend_abort_reason can be placed in interrupt context to retrieve
    the abort reasons.
    
    Bug: 32371978
    Change-Id: I6902770e54b663d21b47289daec19401fc0dbed4

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -159,25 +159,27 @@ int check_wakeup_reason(int irq)
 	int irq_no;
 	int ret = false;
 
-	spin_lock(&resume_reason_lock);
+	unsigned long flags;
+	spin_lock_irqsave(&resume_reason_lock, flags);
 	for (irq_no = 0; irq_no < irqcount; irq_no++)
 		if (irq_list[irq_no] == irq) {
 			ret = true;
 			break;
 	}
-	spin_unlock(&resume_reason_lock);
+	spin_unlock_irqrestore(&resume_reason_lock, flags);
 	return ret;
 }
 
 void log_suspend_abort_reason(const char *fmt, ...)
 {
 	va_list args;
+	unsigned long flags;
 
-	spin_lock(&resume_reason_lock);
+	spin_lock_irqsave(&resume_reason_lock, flags);
 
 	//Suspend abort reason has already been logged.
 	if (suspend_abort) {
-		spin_unlock(&resume_reason_lock);
+		spin_unlock_irqrestore(&resume_reason_lock, flags);
 		return;
 	}
 
@@ -185,13 +187,16 @@ void log_suspend_abort_reason(const char *fmt, ...)
 	va_start(args, fmt);
 	vsnprintf(abort_reason, MAX_SUSPEND_ABORT_LEN, fmt, args);
 	va_end(args);
-	spin_unlock(&resume_reason_lock);
+
+	spin_unlock_irqrestore(&resume_reason_lock, flags);
 }
 
 /* Detects a suspend and clears all the previous wake up reasons*/
 static int wakeup_reason_pm_event(struct notifier_block *notifier,
 		unsigned long pm_event, void *unused)
 {
+	unsigned long flags;
+
 #if IS_ENABLED(CONFIG_SUSPEND_TIME)
 	ktime_t temp;
 	struct timespec suspend_time;
@@ -199,11 +204,11 @@ static int wakeup_reason_pm_event(struct notifier_block *notifier,
 
 	switch (pm_event) {
 	case PM_SUSPEND_PREPARE:
-		spin_lock(&resume_reason_lock);
+		spin_lock_irqsave(&resume_reason_lock, flags);
 		irqcount = 0;
 		suspend_abort = false;
 		mbox_wakeup = false;
-		spin_unlock(&resume_reason_lock);
+		spin_unlock_irqrestore(&resume_reason_lock, flags);
 		/* monotonic time since boot */
 		last_monotime = ktime_get();
 		/* monotonic time since boot including the time spent in suspend */
