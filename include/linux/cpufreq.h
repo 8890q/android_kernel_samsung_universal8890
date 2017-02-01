49b8910fdb3c1c1bc6c53a5d5354d65519f364f0
Andres Oportus <andresoportus@google.com>
2017-02-01 13:34:54 -0800

    ANDROID: cpufreq_stat: add per task/uid/freq stats
    Adds per process nodes in /proc/PID/time_in_state showing per
    frequency times and adds a global /proc/uid_time_in_state
    showing per frequency per uid times.
    
    Bug: 34133340
    Bug: 38320164
    
    Tests: boot sailfish and reading /proc/uid_time_in_state and
    /proc/$$/time_in_state
    
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Change-Id: Ideb22b608b9a5e7bd2200a3a6df0f110b635f96a

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -618,6 +618,10 @@ int cpufreq_generic_init(struct cpufreq_policy *policy,
 #ifdef CONFIG_CPU_FREQ_STAT
 
 void acct_update_power(struct task_struct *p, cputime_t cputime);
+void cpufreq_task_stats_init(struct task_struct *p);
+void cpufreq_task_stats_exit(struct task_struct *p);
+int  proc_time_in_state_show(struct seq_file *m, struct pid_namespace *ns,
+			     struct pid *pid, struct task_struct *p);
 
 #else
 
