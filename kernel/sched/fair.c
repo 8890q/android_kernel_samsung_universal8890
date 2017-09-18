523233d00401a7a978c01e7bccd4be237601023c
Jesse Chan <jc@lineageos.org>
2017-09-17 23:27:36 -0700

    Revert "sched/fair: Modify the comment about lock assumptions in migrate_task_rq_fair()"
    This reverts commit 807874be7a96e9ffce86ae090407c1f3e158b22b.
    
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4784,7 +4784,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 /*
  * Called immediately before a task is migrated to a new cpu; task_cpu(p) and
  * cfs_rq_of(p) references at time of call are still valid and identify the
- * previous cpu. The caller guarantees p->pi_lock or task_rq(p)->lock is held.
+ * previous cpu.  However, the caller only guarantees p->pi_lock is held; no
+ * other assumptions, including the state of rq->lock, should be made.
  */
 static void migrate_task_rq_fair(struct task_struct *p, int next_cpu)
 {
