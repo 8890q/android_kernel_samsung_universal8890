43aac41eca97a818324874cad2b67c4fa0380d2d
Jesse Chan <jc@lineageos.org>
2017-09-17 23:20:36 -0700

    Revert "sched: Calculate energy consumption of sched_group"
    This reverts commit baa90020bf80c8151fe5ef0f138009d41339b41d.
    
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5751,7 +5751,6 @@ DEFINE_PER_CPU(struct sched_domain *, sd_numa);
 DEFINE_PER_CPU(struct sched_domain *, sd_busy);
 DEFINE_PER_CPU(struct sched_domain *, sd_asym);
 DEFINE_PER_CPU(struct sched_domain *, sd_ea);
-DEFINE_PER_CPU(struct sched_domain *, sd_scs);
 
 static void update_top_cache_domain(int cpu)
 {
@@ -5785,9 +5784,6 @@ static void update_top_cache_domain(int cpu)
 			break;
 	}
 	rcu_assign_pointer(per_cpu(sd_ea, cpu), ea_sd);
-
-	sd = highest_flag_domain(cpu, SD_SHARE_CAP_STATES);
-	rcu_assign_pointer(per_cpu(sd_scs, cpu), sd);
 }
 
 /*
