bae6664d7148479a17a571fdb4c50e699a77b66f
=Noxxxious <f.catzgerald@gmail.com>
2019-03-30 07:26:57 +0400

    Selinux: remove sec selinux

--- security/selinux_n/netif.c
+++ security/selinux_n/netif.c
@@ -276,12 +276,6 @@ static struct notifier_block sel_netif_netdev_notifier = {
 static __init int sel_netif_init(void)
 {
 	int i;
-	
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enabled = 1;
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 
 	if (!selinux_enabled)
 		return 0;
--- security/selinux_n/netnode.c
+++ security/selinux_n/netnode.c
@@ -303,12 +303,6 @@ void sel_netnode_flush(void)
 static __init int sel_netnode_init(void)
 {
 	int iter;
-	
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enabled = 1;
-#endif
-// ] SEC_SELINUX_PORTING_COMMON
 
 	if (!selinux_enabled)
 		return 0;
