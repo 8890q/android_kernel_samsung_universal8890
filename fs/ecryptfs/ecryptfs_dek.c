6b52036e3744e0d56eb4319558f03844f60e8b5b
Anan Jaser <ananjaser@gmail.com>
2020-09-29 11:29:54 +0400

    fs: encryptfs: fix misleading intendation
    fs/ecryptfs/ecryptfs_dek.c:371:2: error: misleading indentation; statement is not part of the previous 'if' [-Werror,-Wmisleading-indentation]
            mutex_lock(&crypt_stat->cs_mutex);

--- fs/ecryptfs/ecryptfs_dek.c
+++ fs/ecryptfs/ecryptfs_dek.c
@@ -365,8 +365,8 @@ static int ecryptfs_update_crypt_flag(struct dentry *dentry, enum sdp_op operati
      * To update metadata we need to make sure keysig_list contains fekek.
      * Because our EDEK is stored along with key for protected file.
      */
-    if(list_empty(&crypt_stat->keysig_list))
-        ecryptfs_dek_copy_mount_wide_sigs_to_inode_sigs(crypt_stat, mount_crypt_stat);
+	if (list_empty(&crypt_stat->keysig_list))
+		ecryptfs_dek_copy_mount_wide_sigs_to_inode_sigs(crypt_stat, mount_crypt_stat);
 
 	mutex_lock(&crypt_stat->cs_mutex);
 	rc = ecryptfs_get_lower_file(dentry, inode);
