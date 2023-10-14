a6ed265bafd2f22b360c5c666f550322573ed741
F-19-F <58457605+F-19-F@users.noreply.github.com>
2023-10-15 00:00:11 +0400

    selinux: Allow init exec ksud under nosuid
    For KernelSU modules support on ≤ 4.14 kernels

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2349,12 +2349,34 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
 	int rc;
+	
+#ifdef CONFIG_KSU
+        static u32 ksu_sid;
+        char *secdata;
+        int error;
+        u32 seclen;
+#endif
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
+	
+
+#ifdef CONFIG_KSU
+	if (!ksu_sid) {
+            security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
+	}
+	error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);
+	if (!error) {
+            rc = strcmp("u:r:init:s0",secdata);
+            security_release_secctx(secdata, seclen);
+            if (rc == 0 && new_tsec->sid == ksu_sid) {
+		return 0;
+            }
+        }
+#endif
 
 	/*
 	 * The only transitions we permit under NNP or nosuid
